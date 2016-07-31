a1e7899ff859650bceaa5d625f53f7fd0b4aa533
Vo Anh Kiet <zvoanhkietz@gmail.com>
2016-07-31 15:46:29 +0700

    [Add] Test  login

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+
     /**
      * A basic functional test example.
      *
@@ -14,6 +15,8 @@ class ExampleTest extends TestCase
     public function testBasicExample()
     {
         $this->visit('/')
-             ->see('Laravel 5');
+                ->see('Your Application\'s Landing Page')
+                ->see('Welcome');
     }
+
 }
